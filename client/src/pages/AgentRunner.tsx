686f7eef9c6049b64ef1b71572eef717f7c7143b
tajmalnas <tajjuddin.malnas21@pccoepune.org>
2025-05-31 00:00:36 +0530

    made some changes

--- client/src/pages/AgentRunner.tsx
+++ client/src/pages/AgentRunner.tsx
@@ -92,7 +92,7 @@ export default function AgentRunner() {
     setResponse("");
   
     try {
-      const res = await fetch("http://localhost:4000/run", {
+      const res = await fetch("https://alpha-agent-runner.vercel.app/run", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ prompt, tool }),
